4179c265aecabd7af644e9a30526df29a17071d1
HolOoDoC <BessonF@yandex.ru>
2025-03-18 00:57:48 +0600

    ver1

--- scenes/mini-games/cleanup_toys/node_2d.gd
+++ scenes/mini-games/cleanup_toys/node_2d.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
-	get_children()[randi() % get_child_count()].show() # Replace with function body.
-
+	var _index : int = randi() % get_child_count()
+	print(_index)
+	get_children()[randi() % get_child_count()].show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
