6ade2c8f768809bfa3c3fa8fa4740b9b52d9faec
Felix <felikotov@gmail.com>
2025-03-17 22:31:42 +0600

    Victory Screen

---
+++ scenes/victory_screen/victory_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+const MAIN_MENU = preload('res://scenes/main_menu/main_menu.tscn')
+const GAME = preload('res://scenes/levels/game.tscn')
+@onready var enter_exit: AnimationPlayer = $EnterExit
+
+func _go_to_main_menu() -> void:
+	get_tree().change_scene_to_packed(MAIN_MENU)
+
+
+func _on_button_main_menu_pressed() -> void:
+	enter_exit.play('disappear')
+
+
+func _on_button_фпфшт_pressed() -> void:
+	enter_exit.play('disappear')
