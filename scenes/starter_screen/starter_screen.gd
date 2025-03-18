31caeaa2f52779a4332a4d1229ee0e0b8db64d6d
Felix <felikotov@gmail.com>
2025-03-19 02:29:55 +0600

    Cleaner transition into  starter_screen from spash

--- scenes/starter_screen/starter_screen.gd
+++ scenes/starter_screen/starter_screen.gd
@@ -4,9 +4,8 @@ extends Control
 
 @export var fade_duration : float = 0.5
 
-@export var loader : BackgroundLoader
 @export var label : RichTextLabel
-
+@export var animation_player : AnimationPlayer
 
  
 func _ready() -> void:
@@ -22,12 +21,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if SceneManager.is_everything_loaded : _on_loading_finished()
 
-func _on_loading_finished() -> void:
-	var _tween : Tween = create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-	
-	_tween.tween_property(label, 'modulate:a', 0.0, fade_duration)
-	_tween.tween_callback(_to_main_menu)
-
 
 func _to_main_menu() -> void:
 	SceneManager.to_scene(SceneManager.SCENE.MAIN_MENU)
+
+
+
+func _on_loading_finished() -> void:
+	animation_player.play('outro')
